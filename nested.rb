6b55a9a43986c49aca111241d8cee922cd29a0fa
Roberto Medina <robby.medina562@gmail.com>
2019-08-25 21:28:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,9 +20,7 @@ programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	programmer_hash[:alan_kay][:known_for]
-	
+
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -39,7 +37,9 @@ def alan_kay_is_known_for
         }
      }
 end
-
+	
+	programmer_hash[:alan_kay][:known_for]
+	
 def dennis_ritchies_language
 	programmer_hash = 
  		{
